20f4c4112d32afa7d631db1222e07370df8efe35
dnl092 <drogalsky14@my.whitworth.edu>
2019-06-06 12:37:49 -0700

    added divs and buttons for the game to function

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+//TODO:when start button pressed bring up first question and start timer
+//TODO:when the question is answered bring up either a right or wrong screen for a sec then move one
+//TODO:after 10 questions give them a final score and put up a restart button
